420b1911e3fdd08788735121c0f28525dd04ba9f
vijendra2244 <vs230267@gmail.com>
2024-02-02 13:01:19 +0530

    Authorization middleware is completed successfully

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken");
+const BlackListModel = require("../models/blacklistmodels");
+const dotenv = require("dotenv")
+dotenv.config()
+
+const auth = async (req, res, next) => {
+  const access_token = req.cookies["access_token"];
+  const refresh_token = req.cookies["refresh_token"];
+
+  try {
+    const isBlackListedToken = await BlackListModel.exists({
+      token: access_token,
+    });
+
+    if (isBlackListedToken) {
+      return res.status(400).send("Token is expired, please login again");
+    }
+
+    jwt.verify(access_token, process.env.ACCESS_SECRET_KEY, (err, decoded) => {
+      if (err) {
+        if (err.message === "jwt expired") {
+          jwt.verify(refresh_token, process.env.REFRESH_SECRET_KEY , async (err, refreshDecoded) => {
+            if (err) {
+              return res
+                .status(400)
+                .send("Refresh token is invalid or expired");
+            } else {
+              const access_token = jwt.sign({  _id: refreshDecoded.userId }, "auth", {
+                expiresIn: "1d",
+              });
+
+              res.cookie("access_token", access_token);
+              res.status(200).send("user access");
+              next();
+            }
+          });
+        }
+      } else {
+           req.body.userId= decoded.userId
+           req.body.username = decoded.username
+        next();
+      }
+    });
+  } catch (error) {
+    res.status(400).send("Please login again");
+  }
+};
+
+module.exports ={ auth};
