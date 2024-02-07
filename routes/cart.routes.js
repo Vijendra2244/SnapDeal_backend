ec1914df88ef6d982e3803044a7b4e99474e1b36
vijendra2244 <vs230267@gmail.com>
2024-02-07 13:56:07 +0530

    Creating a cart

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getUserAllCartData,
   addCartInUserCartModel,
+  deleteCartInCartModel
 } = require("../controllers/cart.controllers");
 
 const cartRouter = express.Router();
 
 cartRouter.route("/").get(getUserAllCartData);
 cartRouter.route("/addToCart").post(addCartInUserCartModel);
+cartRouter.route("/deleteCart").post(deleteCartInCartModel)
 
 module.exports = { cartRouter };
